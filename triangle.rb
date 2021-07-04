aee37fbf641dbaf4fd430975e120811f7bb61e1e
Ivan Reyes <ivanwilf@hotmail.com>
2021-07-04 16:03:44 +0900

    triangle projecr cleared

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,11 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  if a == b && b == c
+  if a <= 0 || b <= 0 || c <= 0
+    raise TriangleError, "Illegal triangle"
+  elsif a + b <= c || a + c <= b
+    raise TriangleError, "Illegal triangle"
+  elsif a == b && b == c
     :equilateral
   elsif a != b && a != c && b != c
     :scalene
